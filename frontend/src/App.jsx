27d222a69710264f558b64f8ef843bec47abc881
Arinze Emelobe <arinzeemelobe17@gmail.com>
2026-04-22 14:21:57 +0100

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Invoices from './pages/Invoices';
+import InvoiceDetails from './pages/InvoiceDetails';
+import InvoiceForm from './components/InvoiceForm';
+import Navbar from './components/Navbar';
+import { InvoiceProvider } from './context/InvoiceContext';
+import { ThemeProvider } from './context/ThemeContext';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <InvoiceProvider>
+        <Router>
+          <div className="min-h-screen bg-background">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/invoices/:id" element={<InvoiceDetails />} />
+              <Route path="/invoices/new" element={<InvoiceForm />} />
+              <Route path="/invoices/edit/:id" element={<InvoiceForm />} />
+            </Routes>
+          </div>
+        </Router>
+      </InvoiceProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
