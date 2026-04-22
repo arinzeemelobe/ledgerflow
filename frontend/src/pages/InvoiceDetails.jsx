f1ebdb6435209e4d1cd9b815c671310069d53ed6
Arinze Emelobe <arinzeemelobe17@gmail.com>
2026-04-22 15:09:14 +0100

    Update InvoiceDetails.jsx

--- frontend/src/pages/InvoiceDetails.jsx
+++ frontend/src/pages/InvoiceDetails.jsx
@@ -1,3 +1,4 @@
+import { ClockIcon, ExclamationCircleIcon } from '@heroicons/react/24/outline';
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import { useInvoices } from '../context/InvoiceContext';
